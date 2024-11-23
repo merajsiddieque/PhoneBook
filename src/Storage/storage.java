09ce8f6cfcdcb882a7b9f9ffbdacc26cd329b0db
Meraj Alam <merajsiddieque@gmail.com>
2024-11-23 18:44:53 +0530

    ..

---
+++ src/Storage/storage.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Storage;
+
+/**
+ *
+ * @author meraj
+ */
+public class storage 
+{
+    private String user;
+    public storage(String user)
+    {
+        this.user = user;
+        
+    }
+    
+    
+    
+}
